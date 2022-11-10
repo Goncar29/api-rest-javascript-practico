cb4e63d9549e3dc6926d42d6009e7c741ab1f033
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2022-11-10 17:32:30 -0300

    se realiza la navegacion en js, agregando y quitando estilos inactivos

--- src/main.js
+++ src/main.js
@@ -19,7 +19,7 @@ async function getTrendingMoviesPreview(){
 
     const movies = data.results;
     movies.forEach(movie =>{
-        const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList')
+        const trendingPreviewSection = document.querySelector('#trendingPreview .trendingPreview-movieList')
         const movieContainer = document.createElement('div');
         movieContainer.classList.add('movie-container');
 
@@ -29,7 +29,7 @@ async function getTrendingMoviesPreview(){
         movieImg.setAttribute('src', 'https://image.tmdb.org/t/p/w300/' + movie.poster_path);
         
         movieContainer.appendChild(movieImg);
-        trendingPreviewMoviesContainer.appendChild(movieContainer)
+        trendingPreviewSection.appendChild(movieContainer)
     })
     
 }
@@ -65,7 +65,7 @@ async function getCategoriesPreview(){
     console.log({ data });
 
     categories.forEach(category =>{
-        const previewCategoriesContainer = document.querySelector('#categoriesPreview .categoriesPreview-list')
+        const categoriesPreviewList = document.querySelector('#categoriesPreview .categoriesPreview-list')
         const categoryContainer = document.createElement('div');
         categoryContainer.classList.add('category-container');
 
@@ -76,7 +76,7 @@ async function getCategoriesPreview(){
 
         categoryTitle.appendChild(categoryTitleText)
         categoryContainer.appendChild(categoryTitle)
-        previewCategoriesContainer.appendChild(categoryContainer)
+        categoriesPreviewList.appendChild(categoryContainer)
     })
     
 }
